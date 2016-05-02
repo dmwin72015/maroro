a3db5bcd71639baf5122ced08f0080815f789aa3
marourou2016@icloud.com <marourou2016@icloud.com>
2016-05-02 22:14:18 +0800

    后台末班进行中....

--- routes/blog.js
+++ routes/blog.js
@@ -7,29 +7,3 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
-/*
-	菜单	menu
-	id    				唯一标识
-	menu_name			名称
-	menu_level			级别
-	menu_parent_level	父级别菜单
-	menu_parent_id		父级菜单ID
-
-
-	文章 article
-	id  				唯一标识		
-	arti_name			文章名称
-	arti_author			文章作者
-	create_time			创建时间
-	pub_time			发布时间
-	arti_content		文章内容
-	arti_permission		阅读权限
-	arti_category		文章类别
-	read_num			阅读
-	arti_message		留言
-	like_num			喜欢
-
-
-
-
-*/
